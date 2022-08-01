48f0ecea590206cbede5e30c9cfeae2e98eefeb2
Enrilo Ugalde <ugalde_enrilo@bah.com>
2022-08-01 12:37:46 -0700

    set up post api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  root 'hello_world#index'
   get 'hello_world', to: 'hello_world#index'
+  get 'bye_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_191956) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "is_published"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
